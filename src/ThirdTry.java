16a0ba7f3855e3862c3cd396770c347d459cd197
QuotableShip <153222845+QuotableShip@users.noreply.github.com>
2024-02-16 10:26:25 +0100

    # Conflicts: #	src/ClassManagementNoFileHandling.java

--- src/ThirdTry.java
+++ src/ThirdTry.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-
+//checking changes
 public class ThirdTry extends JFrame {
 
     public ThirdTry() {
